0188a7b96ee8805f1d52fd9cf524094746adfcc6
West Wang <west@misfit.com>
2015-10-14 20:48:19 -0700

    add async tests

---
+++ test/async_tests.js
@@ -0,0 +1,41 @@
+var Q = require('Q');
+var chai = require("chai");
+var chaiAsPromised = require("chai-as-promised");
+
+chai.should();
+chai.use(chaiAsPromised);
+
+describe('Array', function() {
+  describe('string compare', function () {
+
+    it('async test string compare - chai as promised', function () {
+      /**
+       *
+       * var chai = require("chai");
+       * var chaiAsPromised = require("chai-as-promised");
+       *
+       * chai.should();
+       * chai.use(chaiAsPromised);
+       */
+      return Q('ok').should.eventually.equal('ok');
+    });
+
+    it('async test string compare - promise', function (done) {
+      return Q('ok').then(function(value) {
+        value.should.equal('ok');
+      })
+      .then(done, done);
+    });
+
+    it('async test string compare', function (done) {
+      return Q('ok').then(function(value) {
+        if (value == 'ok') done();
+        else done(new Error('not equal ok'));
+      })
+    });
+
+    it('sync test string compare', function () {
+      'ok'.should.equal('ok');
+    });
+  });
+});
