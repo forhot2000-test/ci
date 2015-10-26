9d191ea8d7a785d25d3dfceb8d82ed52acfb0e2d
West Wang <west@misfit.com>
2015-10-26 15:36:04 -0700

    try yaml package

--- ci/run.py
+++ ci/run.py
@@ -3,5 +3,25 @@ from setuptools.command.easy_install import main as install
 
 install(['--user', 'PyYAML'])
 
-if os.system('ls -l') != 0:
-	exit(1)
+import yaml
+
+print yaml.load("""
+language: node_js
+
+before_script:
+  - which python
+  - python --version
+
+script:
+  - python ci/run.py
+
+branches:
+  only:
+    - master
+    - develop
+    - /^feature\/.*$/
+
+""")
+
+print yaml.dump({'name': "The Cloak 'Colluin'", 'depth': 5, 'rarity': 45,
+	'weight': 10, 'cost': 50000, 'flags': ['INT', 'WIS', 'SPEED', 'STEALTH']})
